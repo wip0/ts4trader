9b7c48f5ee38fb34362bc47e9461020bc9e9ee0f
Silver Parasol <silverparasol@hotmail.com>
2017-07-10 16:40:05 +0700

    Read data from file

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-import * as os from 'os';
+import * as fs from 'fs';
+import * as path from 'path';
 
-let osType: string = os.type();
-console.log(`Hello to ${osType}.`);
+// Read data from file
+const INPUT_FILE = './input/data.json';
+let fullPathFile: string = path.resolve(__dirname, INPUT_FILE);
+let data: any = fs.readFileSync(fullPathFile, 'utf-8');
+
+// print data
+console.log(data);
+
+
+
