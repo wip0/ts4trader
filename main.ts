fc83592b653291c56c273e924af011aee914ef62
Silver Parasol <silverparasol@hotmail.com>
2017-07-10 17:26:09 +0700

    Introduce interface, function

--- main.ts
+++ main.ts
@@ -1,6 +1,33 @@
 import * as fs from 'fs';
 import * as path from 'path';
+////////////////////////////////////////////////////////////////////////////////////////////////////
+interface Stock {
+    name: string;
+    cost: number;
+    amount: number;
+    price: number;
+}
 
+interface Fx {
+    name: string;
+    action: string;
+    cost: number;
+    amount: number;
+    bid: number;
+    ask: number;
+}
+
+function calculateFromStock(stocks: Stock[]): number {
+    let net: number = 0;
+    for (let stock of stocks) {
+        // todo: Calculate profit/loss
+    }
+    return net;
+}
+
+// todo: Add a new function to calculate from Fx
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // Read data from file
 const INPUT_FILE = './input/data.json';
 let fullPathFile: string = path.resolve(process.cwd(), INPUT_FILE);
@@ -12,15 +39,12 @@ try {
     // print data
     //console.log(data);
 
-    let stocks: any[] = data.stocks;
-    let numStock: number = stocks.length; 
-    console.log(`We have ${numStock} stocks in our portfolio.` );
+    let stocks: Stock[] = <Stock[]> data.stocks;
+    let fxs: Fx[] = <Fx[]> data.fxs;
 
     // todo: Calculate profit/loss of our portfolio
-    for (let i = 0; i < numStock; ++i) {
-        let stock = stocks[i];
-        // console.log(stock);
-    }
+    // calculateFromStock(stocks);
+
 } catch (error) {
     console.error(error);
 }
