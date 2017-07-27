6d3d51507592e089eb07db7a75d4b69d7894808e
Silver Parasol <silverparasol@hotmail.com>
2017-07-27 23:07:16 +0700

    Introduce async, event in Node.js

--- main.ts
+++ main.ts
@@ -1,13 +1,24 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const csv = require('fast-csv');
+////////////////////////////////////////////////////////////////////////////////
+interface HistoricalData {
+    date: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+}
+
+let eurusd: HistoricalData[] = [];
 // Read data from file
-const INPUT_FILE = './input/data.json';
+const INPUT_FILE = './input/EURUSD.csv';
 let fullPathFile: string = path.resolve(__dirname, INPUT_FILE);
-let data: any = fs.readFileSync(fullPathFile, 'utf-8');
-
-// print data
-console.log(data);
-
-
-
+csv.fromPath(INPUT_FILE, { headers: true}).on('data', (data: any) => {
+    //console.log(data);
+    eurusd.push(<HistoricalData> data);
+}).on('end', () => {
+    //console.log('done');
+    // todo: Find the max value and min value from the loaded historical data  
+});
