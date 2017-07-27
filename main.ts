ecdab074e5ff3e962397472dd50206896788aa3d
Silver Parasol <silverparasol@hotmail.com>
2017-07-27 23:25:03 +0700

    Introduce Promise

--- main.ts
+++ main.ts
@@ -11,16 +11,24 @@ interface HistoricalData {
     close: number;
 }
 
-let eurusd: HistoricalData[] = [];
+function loadHistoricalData(filename: string): Promise<HistoricalData[]> {
+    return new Promise<HistoricalData[]>((resolve, reject) => {
+        let prices: HistoricalData[] = [];
+        csv.fromPath(filename, { headers: true}).on('data', (data: HistoricalData) => {
+            prices.push(data);
+        }).on('end', () => {
+            resolve(prices);
+        }).on('error', (error: any) => {
+            reject(error);
+        })
+    });
+}
+////////////////////////////////////////////////////////////////////////////////
 // Read data from file
 const INPUT_FILE = './input/EURUSD.csv';
 let fullPathFile: string = path.resolve(__dirname, INPUT_FILE);
-csv.fromPath(INPUT_FILE, { headers: true}).on('data', (data: any) => {
-    //console.log(data);
-    eurusd.push(<HistoricalData> data);
-}).on('end', () => {
-    //console.log('done');
-    // todo: Find the max value and min value from the loaded historical data  
-}).on('error', (error) => {
-    console.log(error);
+loadHistoricalData(fullPathFile).then((data: HistoricalData[]) => {
+    // todo: Find when the price is the maximum value
+}).catch((error: any) => {
+    console.error(error);
 });
