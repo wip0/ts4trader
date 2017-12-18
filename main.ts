dba74a6e443ec47bffe2cd9969b3b123007d49a2
DESKTOP-I0K1FTH\Krit <kritLaptop_ex08>
2017-12-18 07:39:34 -0800

    extract income level of 1 country ได้แล้ว

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 require('source-map-support').install();
 
+
 import * as request from 'request';
 ///////////////////////////////////////////////////////////////////////////////////////////
 const hostUrl: string = 'http://api.worldbank.org/v2/';
-const countryApiUrl: string = `${hostUrl}/countries`;
+const countryApiUrl: string = `${hostUrl}/countries/TH`;
 
 // https://datahelpdesk.worldbank.org/knowledgebase/articles/898590-api-country-queries
 request.get(`${countryApiUrl}?format=json`)
@@ -12,9 +13,28 @@ request.get(`${countryApiUrl}?format=json`)
     })
     .on('data', (buf: Buffer) => {
         const data: string = buf.toString();
-        //console.log(`Data: ${data}`);
-        // todo: show all countries that have high income
-    })
+        console.log(`Data: ${data}`);
+       let arrayOfmyObj: any = JSON.parse(data);
+       // arrayofmyobj length
+       console.log(arrayOfmyObj.length)
+        for(var i =0; i< arrayOfmyObj.length; i++){
+            var object = arrayOfmyObj[i]
+            //console.log(object)
+            for ( var j =0; j< object.length; j++){
+               var object1 = object[j]
+               console.log(object1)
+               console.log(object1.incomeLevel.value)
+               //for( var k =0; k< object1.length; k++){
+                 //  console.log(object1[k])
+               }
+            
+        }
+    
+    
+    }
+        
+        // todo: show all countries that have high income 
+    )
     .on('error', (error) => {
         console.error(error.message || error);
     });
