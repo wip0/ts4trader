6285e5e63f92d8b7016fde2a9bbac237c46a1314
arbitrage13 <arbitrage13@gmail.com>
2017-12-29 14:19:19 +0700

    only 1st page extraction

--- main.ts
+++ main.ts
@@ -1,20 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 require('source-map-support').install();
-
-import * as request from 'request';
+const request = require("request");
 ///////////////////////////////////////////////////////////////////////////////////////////
-const hostUrl: string = 'http://api.worldbank.org/v2/';
-const countryApiUrl: string = `${hostUrl}/countries`;
-
+const hostUrl = 'http://api.worldbank.org/v2/';
+const countryApiUrl = `${hostUrl}/countries/all`;
+let data = '';
 // https://datahelpdesk.worldbank.org/knowledgebase/articles/898590-api-country-queries
 request.get(`${countryApiUrl}?format=json`)
     .on('response', (res) => {
-        console.log(`HTTP Result = ${res.statusCode} ${res.statusMessage}`);
-    })
-    .on('data', (buf: Buffer) => {
-        const data: string = buf.toString();
-        //console.log(`Data: ${data}`);
-        // todo: show all countries that have high income
-    })
+    console.log(`HTTP Result = ${res.statusCode} ${res.statusMessage}`);
+})
+    .on('data', (buf:Buffer) => {
+    data += buf;
+})
+    .on('end', () => {
+    data = JSON.parse(data);
+    //const data: string = buf.toString();
+    console.log(`Data: ${data}`);
+    let arrayOfmyObj = data;
+    // arrayofmyobj length
+    console.log(arrayOfmyObj.length);
+    for (var i = 0; i < arrayOfmyObj.length; i++) {
+        var object = arrayOfmyObj[i];
+        console.log(object);
+        for (var j = 0; j < object.length; j++) {
+            var incomeLevel = object[j];
+            //console.log(incomeLevel)
+            if (incomeLevel.incomeLevel.value == 'High income') {
+                console.log(`${incomeLevel.incomeLevel.value} : ${object[j].name} : capitalCity : ${object[j].capitalCity}`);
+            }
+        }
+    }
+})
     .on('error', (error) => {
-        console.error(error.message || error);
-    });
+    console.error(error.message || error);
+});
+//# sourceMappingURL=main.js.map
