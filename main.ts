a19b60e666e63725859aae30f1ad3b6b017452dd
arbitrage13 <arbitrage13@gmail.com>
2017-12-29 17:02:19 +0700

    try to use page=x to get data

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
+import { url } from "inspector";
+
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require('source-map-support').install();
-
+let page:number = 1;
 const request = require("request");
 ///////////////////////////////////////////////////////////////////////////////////////////
+
 const hostUrl = 'http://api.worldbank.org/v2/';
-const countryApiUrl = `${hostUrl}/countries?per_page=500`;
+const countryApiUrl = `${hostUrl}/countries?page=`+ page;
 let data = '';
 // https://datahelpdesk.worldbank.org/knowledgebase/articles/898590-api-country-queries
-request.get(`${countryApiUrl}&format=json`)
+
+function fetch(page){
+    var url: string = "http://api.worldbank.org/v2/countries?page=" + page + "&format=json"
+    request.get(url, function(result) {
+        if (page < 7){
+            fetch(page++)
+        }
+    })
+}
+
+
+request.get(url)
     .on('response', (res) => {
     console.log(`HTTP Result = ${res.statusCode} ${res.statusMessage}`);
+    fetch(page);
 })
     .on('data', (buf:Buffer) => {
     data += buf;
