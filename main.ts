01bc1140103624828eaf4aa5926f71126dba0a04
arbitrage13 <arbitrage13@gmail.com>
2018-07-06 08:21:01 +0700

    final final version of map reduce filter forEach

--- main.ts
+++ main.ts
@@ -44,15 +44,12 @@ for(let idx:number =1 ; idx<=10;idx++){
 Promise.all(promises).then(results=>{
     return results.map(stringData=>{return JSON.parse(stringData)
         }).filter(array=>array[0].page<=array[0].pages)
-        }).then(objects=>{
-            return objects.map(array=>{return array[1]
+        .map(array=>{return array[1]
                 }).reduce((total,members)=>{
                     return total.concat(members)
-                }).filter(listOfCountries=>{return listOfCountries.incomeLevel.value == incomeValue
-                }).forEach(countries=>{console.log(`${incomeValue} : ${countries.name}`)})
+                        }).filter(listOfCountries=>{return listOfCountries.incomeLevel.value == incomeValue
+                        }).forEach(countries=>{console.log(`${incomeValue} : ${countries.name}`)})
            
-            
-       
 })
             
     
